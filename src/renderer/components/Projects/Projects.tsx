4f63ad1875ade4f4cf0d947277f9793071fd77fa
Pat Laplante <laplante.patrick@gmail.com>
2020-05-21 23:18:00 -0400

    Switched to electron 9 and added placeholder text

--- src/renderer/components/Projects/Projects.tsx
+++ src/renderer/components/Projects/Projects.tsx
@@ -2,21 +2,45 @@ import * as React from "react";
 import styled from 'styled-components';
 import { Toolbar } from './Toolbar';
 
+interface PlaceholderProps {
+    navToManageProjects(): void;
+}
+
+const Placeholder: React.FC<PlaceholderProps> = ({navToManageProjects}) => {
+    
+    const onClick = (ev: React.MouseEvent): boolean => {
+        navToManageProjects();
+        return false;
+    };
+
+    return (
+        <PlaceholderContainer>
+            <div>
+                You don't have any projects. Use the <a id="myLink" title="Click to do something"
+ href="#" onClick={onClick}>manage projects</a> menu to add a new one.
+            </div>
+        </PlaceholderContainer>
+    );
+};
 
 export const Projects: React.FC = () => {
+
+    const navToManageProjects = React.useCallback((): void => {
+        console.log("BOO")
+    }, []);
 
     const MainContent = (props): JSX.Element => {
         if (props.defaultProject) {
             return (<Baloney>asdsad</Baloney>)
         } else {
-            return (<div />)
+            return (<Placeholder navToManageProjects={navToManageProjects} />)
         }
     };
 
     return (
         <Container2>
-            <Toolbar />
-            <MainContent defaultProject={true} />
+            <Toolbar Title="hello" Body="blabla" navToManageProjects={navToManageProjects} />
+            <MainContent defaultProject={false} />
         </Container2>
     );
 };
@@ -35,4 +59,14 @@ const Baloney = styled.div`
     position: relative;
     flex-grow: 1;
     background-color: blue;
+`;
+
+const PlaceholderContainer = styled.div.attrs(props => ({
+}))`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex-grow: 1;
+    width: 100vw;
 `;
--- src/renderer/components/Projects/Toolbar.tsx
+++ src/renderer/components/Projects/Toolbar.tsx
@@ -1,10 +1,30 @@
 import * as React from "react";
 import styled from 'styled-components';
 
+interface ToolbarProps {
+    Title: string;
+    Image?: string;
+    Body: string;
+    navToManageProjects(): void;
+}
+
+// export const Card: React.FC<CardProps> =
+//     ({ Title, Image, Body }) => (
+//         <div className="card">
+//             <div className="title">{Title}</div>
+//             {Image &&
+//                 <div className="image">
+//                     <img src={Image} />
+//                 </div>
+//             }
+//             <div className="body">{Body}</div>
+//         </div>
+//     )
 
 
-export const Toolbar: React.FC = () => {
-    return (
+
+export const Toolbar: React.FC<ToolbarProps> = 
+    ({ Title, Image, Body, navToManageProjects }) => (
         <Container >
             <ProjectSelection>
                 <option>Coffee</option>
@@ -12,9 +32,10 @@ export const Toolbar: React.FC = () => {
                 <option>Juice</option>
                 <option selected>Cocktail</option>
             </ProjectSelection>
+            <div onClick={navToManageProjects}>{Title}</div>
         </Container>
     );
-};
+
 
 // TODO figure out how to avoid a typescript linting error
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -23,6 +44,8 @@ const Container = styled.div.attrs(props => ({
     width: 100vw;
     height: 61px;
     border-bottom: 1px solid black;
+    display: flex;
+    flex-direction: row;
 `;
 
 const ProjectSelection = styled.select.attrs(props => ({
